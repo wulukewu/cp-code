b028056c0fbe17e3ac87513732733593922a33ee
WULukeWU <68338769+WULukeWU@users.noreply.github.com>
2025-03-23 01:57:03 +0800

    feat: 2024/10/15 CPE q1

---
+++ cpe/2024-10-15/12250_Language_Detection.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <map>
+// #include <algorithm>
+
+using namespace std;
+
+signed main(){
+    int t;
+    cin >> t;
+
+    map < char, int > arr; 
+
+    string s;
+    char v;
+    while(cin >> s){
+        for(int i=0; i<s.size(); i++){
+            v = s[i];
+            if('A' <= v and v <= 'Z'){
+
+            }else if('a' <= v and v <= 'z'){
+                v -= 'a' - 'A';
+            }else{
+                continue;
+            }
+
+            if(arr.find(v) == arr.end()){
+                arr[v] = 1;
+            }else{
+                arr[v]++;
+            }
+        }
+    }
+
+    // for(auto i: arr){
+    //     cout << i.first << ' ' << i.second << endl;
+    // }
+
+    int m = 0;
+    for(auto i: arr){
+        m = max(m, i.second);
+    }
+    
+    vector < vector < char > > ans(m, vector < char >());
+    for(auto i: arr){
+        ans[i.second-1].push_back(i.first);
+    }
+
+    for(int i=ans.size()-1; i>=0; i--){
+        if(ans[i].size() == 0){
+            continue;
+        }
+        for(auto j: ans[i]){
+            cout << j << ' ' << arr[j] << endl;
+        }
+    }
+}

74f3a29dac58b914c90f30b007b38dafecf07a74
WULukeWU <68338769+WULukeWU@users.noreply.github.com>
2025-05-29 18:31:42 +0800

    feat: 2025-05-20 cpe no.5

---
+++ cpe/2025-05-20/357_Let_Me_Count_The_Ways.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+#define int long long
+
+using namespace std;
+
+signed main(){
+    vector < int > dp(3e4+1, 1);
+    int c[4] = {5, 10, 25, 50};
+
+    for(int i=0; i<4; i++){
+        for(int j=0; j<3e4+1; j++){
+            if(j-c[i]>=0){
+                dp[j] += dp[j-c[i]];
+            }
+        }
+    }
+
+    // for(int i=0; i<3e4; i++){
+    //     cout << dp[i] << ' ';
+    // }
+    // cout << endl;
+
+    int n, m;
+    while(cin >> n){
+        m = dp[n];
+        // cout << m << endl;
+        if(m>1){
+            cout << "There are " << m << " ways to produce " << n << " cents change." << endl;
+        }else{
+            cout << "There is only 1 way to produce " << n << " cents change." << endl;
+        }
+    }
+}

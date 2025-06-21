76e7a082f54d093cde6ccea133b0052267f466e1
WULukeWU <68338769+WULukeWU@users.noreply.github.com>
2025-06-21 21:44:43 +0800

    feat: 20250621 atcoder abc411

---
+++ atcoder/abc/abc411/C_Black_Intervals.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <random>
+#include <set>
+#include <map>
+#include <stack>
+#include <math.h>
+#define int long long
+// #define float double
+// #define float long double
+#define F first
+#define S second
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define Fio ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+signed main() {
+    int n, q;
+    cin >> n >> q;
+    
+    vector < int > arr(n, 0);
+
+    int a;
+    int ans = 0;
+    for(int i=0; i<q; i++){
+        cin >> a;
+        a--;
+
+        if(arr[a]==0){
+            arr[a]=1;
+            if(a==0){
+                if(arr[a+1]==0){
+                    ans++;
+                }
+            }else if(a==n-1){
+                if(arr[a-1]==0){
+                    ans++;
+                }
+            }else{
+                if(arr[a-1]==1 and arr[a+1]==1){
+                    ans--;
+                }else if(arr[a-1]==0 and arr[a+1]==0){
+                    ans++;
+                }
+            }
+        }else{
+            arr[a]=0;
+            if(a==0){
+                if(arr[a+1]==0){
+                    ans--;
+                }
+            }else if(a==n-1){
+                if(arr[a-1]==0){
+                    ans--;
+                }
+            }else{
+                if(arr[a-1]==0 and arr[a+1]==0){
+                    ans--;
+                }else if(arr[a-1]==1 and arr[a+1]==1){
+                    ans++;
+                }
+            }
+        }
+
+        cout << ans << endl;
+    }
+}
---
+++ atcoder/abc/abc411/D_Conflict_2.cpp
@@ -0,0 +1,85 @@
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <queue>
+#include <random>
+#include <set>
+#include <map>
+#include <stack>
+#include <math.h>
+#define int long long
+// #define float double
+// #define float long double
+#define F first
+#define S second
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define Fio ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using namespace std;
+
+signed main() {
+    int n, q;
+    cin >> n >> q;
+
+    vector < vector < int > > arr(n);
+    vector < int > brr;
+    vector < string > crr;
+    int idx = -1;
+
+    int a, p;
+    string s;
+    while(q--){
+        cin >> a >> p;
+        p--;
+
+        if(a==1){
+            arr[p] = brr;
+        }else if(a==2){
+            cin >> s;
+            crr.push_back(s);
+            idx++;
+            arr[p].push_back(idx);
+        }else{
+            brr = arr[p];
+        }
+    }
+
+    // for(auto i: crr){
+    //     cout << i << ' ';
+    // }
+    // cout << endl;
+
+    for(auto i: brr){
+        cout << crr[i];
+    }
+
+    // vector < string > arr(n, "");
+
+    // string ans = "";
+
+    // int a, p;
+    // string s;
+    // while(q--){
+    //     cin >> a >> p;
+    //     p--;
+    //     // cout << a << ' ' << p << endl;
+
+    //     if(a==1){
+    //         arr[p] = ans;
+    //     }else if(a==2){
+    //         cin >> s;
+    //         arr[p] += s;
+    //     }else{
+    //         ans = arr[p];
+    //     }
+
+    //     // cout << ans << '_';
+    //     // for(int i=0; i<n; i++){
+    //     //     cout << arr[i] << '_';
+    //     // }
+    //     // cout << endl;
+    // }
+
+    // cout << ans;
+}

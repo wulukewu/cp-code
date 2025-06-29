b6659656b8b2e869d1bfae491ffdbc99a4a89737
WULukeWU <68338769+WULukeWU@users.noreply.github.com>
2025-06-29 15:53:42 +0800

    feat: 20250629 01 背包問題

---
+++ zerojudge/b116_TOI2008_3_加減問題.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int m, n;
+    while(cin >> m){
+        cin >> n;
+        while(m--){
+            vector < int > a(n);
+            for(int i=0; i<n; i++){
+                cin >> a[i];
+            }
+
+            int sum_a = 0;
+            for(int i=0; i<n; i++){
+                sum_a += a[i];
+            }
+
+            int k = sum_a / 2;
+            vector < int > dp(k+1, 0);
+            for(int i=0; i<n; i++){
+                for(int j=k; j-a[i]>=0; j--){
+                    dp[j] = max(dp[j], dp[j-a[i]]+a[i]);
+                }
+            }
+
+            // for(int i=0; i<=k; i++){
+            //     cout << dp[i] << ' ';
+            // }
+            // cout << endl;
+
+            if(dp[k]*2==sum_a){
+                cout << "Yes" << endl;
+            }else{
+                cout << "No" << endl;
+            }
+        }
+    }
+}
---
+++ zerojudge/d390_00562_Dividing_coins.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+
+        vector < int > c(n);
+        for(int i=0; i<n; i++){
+            cin >> c[i];
+        }
+
+        int sum_c = 0;
+        for(int i=0; i<n; i++){
+            sum_c += c[i];
+        }
+
+        int k = sum_c / 2;
+        vector < int > dp(k+1, 0);
+        for(int i=0; i<n; i++){
+            for(int j=k; j-c[i]>=0; j--){
+                dp[j] = max(dp[j], dp[j-c[i]]+c[i]);
+            }
+        }
+
+        // for(int i=0; i<=k; i++){
+        //     cout << dp[i] << ' ';
+        // }
+        // cout << endl;
+
+        cout << sum_c - dp[k]*2 << endl;
+    }
+}

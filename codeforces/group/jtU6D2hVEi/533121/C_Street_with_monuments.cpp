e494007dd3d61f5e91b1035c512975c70012a7ff
wulukewu <lukewu1015@gmail.com>
2025-07-11 11:14:43 +0800

    feat: 20250611 codeforces practice

---
+++ codeforces/group/jtU6D2hVEi/533121/C_Street_with_monuments.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define F first 
+#define S second
+#define ALL(v) v.begin(),v.end()
+#define EB emplace_back
+#define PB push_back
+#define endl '\n'
+#define int long long
+//#define float double
+//#define float long double
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+//#define Fio ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using uint = unsigned;
+using ull = unsigned long long;
+using ll = long long;
+using ld = long double;
+template<typename T> using Prior = priority_queue<T>;
+template<typename T> using prior = priority_queue<T, vector<T>, greater<T>>;
+const ll MOD = 1e9 + 7;
+const double EPS = 1e-9;
+void addmod(ll &a, ll b) {a = (a+b)%MOD;}
+void submod(ll &a, ll b) {a = (a-b+MOD)%MOD;} 
+void timesmod(ll &a, ll b) {a = (a*b)%MOD;}
+ll POW(ll a, ll b) {ll res=1; do{if(b%2)timesmod(res,a);timesmod(a,a);}while(b>>=1); return res;}
+
+void solve() {
+    int n, r;
+    cin >> n >> r;
+
+    vector<int>d(n);
+    FOR(i, 0, n) cin >> d[i];
+
+    int ans = 0;
+    FOR(i, 0, n){
+        auto it_upper = upper_bound(ALL(d), d[i]+r);
+        // cout << d.end()-it_upper << ' ' << *it_upper << endl;
+        ans += d.end()-it_upper;
+    }
+
+    cout << ans << endl;
+}
+
+signed main() {
+    ios::sync_with_stdio(false),cin.tie(0);
+    int t = 1;
+    //cin >> t; 
+    while (t--) solve();
+    return 0;
+}

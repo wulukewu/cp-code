132d8fa7d7468aadde87c8bc5bc6d531d0e86562
wulukewu <lukewu1015@gmail.com>
2025-07-27 16:58:50 +0800

    feat: 20250727 codeforces week 04 practice

---
+++ codeforces/group/jtU6D2hVEi/533371/E_Badgers.cpp
@@ -0,0 +1,91 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define F first 
+#define S second
+#define ALL(v) v.begin(),v.end()
+#define EB emplace_back
+#define PB push_back
+#define endl '\n'
+//#define int long long
+//#define float double
+//#define float long double
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+//#define Fio ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using uint = unsigned;
+using ull = unsigned long long;
+using ll = long long;
+using ld = long double;
+template<typename T> using Prior = priority_queue<T>;
+template<typename T> using prior = priority_queue<T, vector<T>, greater<T>>;
+const ll MOD = 1e9 + 7;
+const double EPS = 1e-9;
+void addmod(ll &a, ll b) {a = (a+b)%MOD;}
+void submod(ll &a, ll b) {a = (a-b+MOD)%MOD;} 
+void timesmod(ll &a, ll b) {a = (a*b)%MOD;}
+ll POW(ll a, ll b) {ll res=1; do{if(b%2)timesmod(res,a);timesmod(a,a);}while(b>>=1); return res;}
+template<typename T>
+void print(T a){
+    for(auto u: a){
+        cout << u << ' ';
+    }
+    cout << endl;
+}
+template <typename T>
+void printQ(queue < T > q){
+    while(!q.empty()){
+        cout << q.top() << " ";
+        q.pop();
+    }
+    cout << endl;
+}
+template <typename T>
+void printPQ(priority_queue < T > pq){
+    while(!pq.empty()){
+        cout << pq.top() << " ";
+        pq.pop();
+    }
+    cout << endl;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vector < pair < int, int > > v(n);
+    FOR(i, 0, n) cin >> v[i].F;
+    FOR(i, 0, n) cin >> v[i].S;
+
+    int t;
+    cin >> t;
+
+    int ans = 0;
+    FOR(k, 1, n+1){
+        vector < int > costs(n);
+        FOR(i, 0, n){
+            costs[i] = v[i].F + v[i].S * (k-1);
+        }
+        sort(ALL(costs));
+
+        int total = 0;
+        FOR(i, 0, k){
+            total += costs[i];
+        }
+
+        if(total <= t){
+            ans = k;
+        }else{
+            break;
+        }
+    }
+
+    cout << ans << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false),cin.tie(0);
+    int t = 1;
+    //cin >> t; 
+    while (t--) solve();
+    return 0;
+}

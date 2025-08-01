64c9879e416ddc368a0ea6e5c4de653fb5feb091
wulukewu <lukewu1015@gmail.com>
2025-08-01 21:49:09 +0800

    codeforces dp 2 practice

---
+++ codeforces/group/jtU6D2hVEi/533280/E_Knight.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define F first 
+#define S second
+#define ALL(v) v.begin(),v.end()
+#define EB emplace_back
+#define PB push_back
+#define endl '\n'
+//#define int long long
+//#define float double
+//#define float long double
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+//#define Fio ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using uint = unsigned;
+using ull = unsigned long long;
+using ll = long long;
+using ld = long double;
+template<typename T> using Prior = priority_queue<T>;
+template<typename T> using prior = priority_queue<T, vector<T>, greater<T>>;
+const ll MOD = 1e9 + 7;
+const double EPS = 1e-9;
+void addmod(ll &a, ll b) {a = (a+b)%MOD;}
+void submod(ll &a, ll b) {a = (a-b+MOD)%MOD;} 
+void timesmod(ll &a, ll b) {a = (a*b)%MOD;}
+ll POW(ll a, ll b) {ll res=1; do{if(b%2)timesmod(res,a);timesmod(a,a);}while(b>>=1); return res;}
+template<typename T>
+void print(T a){
+    for(auto u: a){
+        cout << u << ' ';
+    }
+    cout << endl;
+}
+template <typename T>
+void printQ(queue < T > q){
+    while(!q.empty()){
+        cout << q.top() << " ";
+        q.pop();
+    }
+    cout << endl;
+}
+template <typename T>
+void printPQ(priority_queue < T > pq){
+    while(!pq.empty()){
+        cout << pq.top() << " ";
+        pq.pop();
+    }
+    cout << endl;
+}
+
+void solve() {
+    int n, m;
+    cin >> n >> m;
+
+    int offs[2][2] = {{-1, -2}, {-2, -1}};
+    vector < vector < int > > dp(n, vector < int > (m, 0));
+    dp[0][0] = 1;
+    FOR(i, 0, n){
+        FOR(j, 0, m){
+            FOR(k, 0, 2){
+                int x = i + offs[k][0];
+                int y = j + offs[k][1];
+                if(x>=0 and x<n and y>=0 and y<m){
+                    dp[i][j] += dp[x][y];
+                }
+            }
+        }
+    }
+
+    // FOR(i, 0, n){
+    //     FOR(j, 0, m){
+    //         cout << dp[i][j] << ' ';
+    //     }
+    //     cout << endl;
+    // }
+
+    cout << dp[n-1][m-1] << endl;
+}
+
+int main() {
+    ios::sync_with_stdio(false),cin.tie(0);
+    int t = 1;
+    //cin >> t; 
+    while (t--) solve();
+    return 0;
+}

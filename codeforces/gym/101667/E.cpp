8a445325c0cf8f076723a4d1fbfb467cb292ccca
wulukewu <lukewu1015@gmail.com>
2025-09-19 15:36:32 +0800

    icpc practice lzr

---
+++ codeforces/gym/101667/E.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+#define ALL(v) v.begin(), v.end()
+#define PB push_back 
+
+solve(){
+    int n, q, l, r;
+    cin >> n >> q >> l >> r;
+    vector<int> a;
+
+    FOR(i, 0, n){
+        int ai;
+        cin >> ai;
+        a.PB(ai);
+    }
+
+    sort(ALL(v));
+
+    FOR(i, 0, q){
+        int b;
+        cin >> b;
+
+        int rightwatch, rightshoot, leftwatch, leftshoot;
+        rightwatch = 
+    }
+
+
+    
+}
+
+int main(){
+    ios::sync_with_stdio(false), cin.tie(0);
+    int t = 1;
+    // cin >> t;
+    while(t--) solve();
+    return 0;
+}

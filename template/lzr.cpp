781549ba320a5012bf7682bf06c82e72eb31fad0
wulukewu <lukewu1015@gmail.com>
2025-10-11 21:44:19 +0800

    fix: remove space from template

--- template/lzr.cpp
+++ template/lzr.cpp
@@ -50,7 +50,7 @@ void solve() {
 int main() {
     ios::sync_with_stdio(false),cin.tie(0);
     int t = 1;
-    //cin >> t; 
+    //cin >> t;
     while (t--) solve();
     return 0;
 }
--- template/template.cpp
+++ template/template.cpp
@@ -55,7 +55,7 @@ void solve() {
 int main() {
     ios::sync_with_stdio(false),cin.tie(0);
     int t = 1;
-    //cin >> t; 
+    //cin >> t;
     while (t--) solve();
     return 0;
 }

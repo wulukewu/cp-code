1dd2ef5cea742a9f1fc94e17cfb6f7669df78d5c
wulukewu <lukewu1015@gmail.com>
2026-01-14 00:16:35 +0800

    add: ncumaoj practice

---
+++ ncuma-oj/ZA06_上班是快樂的嗎.cpp
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define F first 
+#define S second
+#define ALL(v) v.begin(),v.end()
+#define EB emplace_back
+#define PB push_back
+#define endl '\n'
+//#define int long long
+//#define float double
+//#define float long double
+#define FOR(i, a, b) for(int i = a; i < b; i++)
+//#define Fio ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+using uint = unsigned;
+using ull = unsigned long long;
+using ll = long long;
+using ld = long double;
+template<typename T> using Prior = priority_queue<T>;
+template<typename T> using prior = priority_queue<T, vector<T>, greater<T>>;
+const ll MOD = 1e9 + 7;
+const double EPS = 1e-9;
+void addmod(ll &a, ll b) {a = (a+b)%MOD;}
+void submod(ll &a, ll b) {a = (a-b+MOD)%MOD;} 
+void timesmod(ll &a, ll b) {a = (a*b)%MOD;}
+ll POW(ll a, ll b) {ll res=1; do{if(b%2)timesmod(res,a);timesmod(a,a);}while(b>>=1); return res;}
+void dividemod(ll &a, ll b){a = a * POW(b, MOD-2) % MOD;}
+template<typename T>
+void print(T a){
+    for(auto u: a){
+        cout << u << ' ';
+    }
+    cout << endl;
+}
+template <typename T>
+void printQ(queue < T > q){
+    while(!q.empty()){
+        cout << q.top() << " ";
+        q.pop();
+    }
+    cout << endl;
+}
+template <typename T>
+void printPQ(priority_queue < T > pq){
+    while(!pq.empty()){
+        cout << pq.top() << " ";
+        pq.pop();
+    }
+    cout << endl;
+}
+
+void solve(){
+    int n;
+    while(cin >> n){
+        bool ans = false;
+        set<int>st;
+        while(true){
+            st.insert(n);
+            int m = 0;
+            while(n){
+                int a = n % 10;
+                m += a * a;
+                n /= 10;
+            }
+            // cout << m << endl;
+            n = m;
+
+            if(n==1){
+                ans = true;
+                break;
+            }
+            if(st.find(n)!=st.end()){
+                break;
+            }
+        }
+        if(ans){
+            cout << "Happy" << endl;
+        }else{
+            cout << "Unhappy" << endl;
+        }
+    }
+}
+
+int main(){
+    ios::sync_with_stdio(false),cin.tie(0);
+    int t = 1;
+    //cin >> t;
+    while (t--) solve();
+    return 0;
+}
